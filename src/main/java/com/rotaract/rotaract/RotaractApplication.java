ce8fa1718a09b14d200ce3daf04e53cf9cac0f3b
Unknown <blanchedelaferte@hotmail.fr>
2018-06-03 12:36:01 +0200

    proute

--- src/main/java/com/rotaract/rotaract/RotaractApplication.java
+++ src/main/java/com/rotaract/rotaract/RotaractApplication.java
@@ -1,12 +1,15 @@
 package com.rotaract.rotaract;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RotaractApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RotaractApplication.class, args);
 	}
-}
+	}

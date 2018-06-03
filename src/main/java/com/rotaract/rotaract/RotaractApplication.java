c9e337fadb8b466871d47dd9bc08795051becd6a
Unknown <blanchedelaferte@hotmail.fr>
2018-06-03 11:21:46 +0200

    spring boot init

---
+++ src/main/java/com/rotaract/rotaract/RotaractApplication.java
@@ -0,0 +1,12 @@
+package com.rotaract.rotaract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaractApplication.class, args);
+	}
+}
